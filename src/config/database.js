b552e0ef9a11887b7ed62ec592d41f1457cb703e
jefshandler <jef_shandler@msn.com>
2022-04-21 22:30:22 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,61 @@
+// require("dotenv").config({
+//   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+// });
+module.exports = {
+  host: "127.0.0.1",
+  username: "root",
+  password: "root",
+  database: "_prod",
+  dialect: "mysql",
+  operatorAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+  // host: process.env.DB_HOST,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
+  // dialect: process.env.DB_DIALECT || "mysql",
+  // //storage: './__tests__/database.sqlite',
+  // operationAliases: false,
+  // logging: false,
+  // define: {
+  //   timestamps: true,
+  //   underscored: true,
+  //   underscoredAll: true
+  // }
+};
+
+
+
+
+
+
+
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const routes = require("express").Router();
+// const { User } =require("./app/models");
+//
+// User.create({
+//     name: "Noah1",
+//     email: "noah1@b.com",
+//     password_hash: "123654789"
+// });
+
+//definir rotas
+
+
+module.exports = routes;
